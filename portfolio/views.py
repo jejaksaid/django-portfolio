c06017ea9259b5b9b622d54059d233e54096e636
jejaksaid <said@fastlearningcenter.com>
2021-06-09 17:22:20 +0700

    some codes

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Images
+
 
 def home(request):
-
-    return render(request, "index.html")
+    context = {}
+    images = Images.objects.all()
+    context["images"] = images
+    return render(request, "index.html", context)
